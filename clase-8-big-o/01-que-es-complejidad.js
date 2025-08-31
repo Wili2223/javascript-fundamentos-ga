a5c997d740f15e430037bcd7c99fd730c846cba3
Wili2223 <williamsvalle2@gmail.com>
2025-08-31 09:49:12 -0300

    ejercicio que es complejidad terminada

--- clase-8-big-o/01-que-es-complejidad.js
+++ clase-8-big-o/01-que-es-complejidad.js
@@ -6,17 +6,27 @@
 //--------------------------------------------------
 // 👉 Paso 1: Escribí con tus palabras en un comentario qué es la complejidad algorítmica y para qué sirve.
 
+//Es una forma de medir el rendimiento de un algoritmo en función de la cantidad de recursos que utiliza, 
+//como el tiempo de ejecución y la memoria que consume. Su principal propósito es ayudarnos a predecir 
+//cómo se comportará un algoritmo a medida que el tamaño de los datos de entrada aumenta.
+
 //--------------------------------------------------
 // 2. Reflexión
 //--------------------------------------------------
 // 👉 Paso 2: ¿Por qué es importante comparar la eficiencia de diferentes algoritmos?
 // Respondé en comentario.
 
+//Es crucial comparar la eficiencia de los algoritmos para elegir la mejor solución para un problema dado.
+ //No todos los algoritmos son igualmente eficientes. Un algoritmo que funciona bien con un pequeño conjunto 
+ //de datos podría volverse extremadamente lento o consumir demasiada memoria cuando se enfrenta a un conjunto
+  //de datos grande.  Al comparar la complejidad, podemos prever cuál de las soluciones tendrá el mejor 
+  //rendimiento a gran escala, lo que es vital para construir aplicaciones robustas y escalables
+
 //--------------------------------------------------
 // 3. Múltiple choice
 //--------------------------------------------------
 // ❓ ¿Qué mide la complejidad algorítmica?
-let respuestaMC1;
+let respuestaMC1 = 'A';
 // respuestaMC1 = 'A'; // A. Qué tan eficiente es un algoritmo
 // respuestaMC1 = 'B'; // B. El lenguaje de programación
 // respuestaMC1 = 'C'; // C. El diseño visual
@@ -24,8 +34,8 @@ let respuestaMC1;
 //--------------------------------------------------
 // 4. Verdadero o falso
 //--------------------------------------------------
-let respuestaVF1; // "La complejidad algorítmica solo mide el tiempo, no la memoria"
-let respuestaVF2; // "Comparar algoritmos ayuda a elegir mejores soluciones"
+let respuestaVF1 = false; // "La complejidad algorítmica solo mide el tiempo, no la memoria"
+let respuestaVF2 = true; // "Comparar algoritmos ayuda a elegir mejores soluciones"
 
 //--------------------------------------------------
 // No modifiques el código debajo de este comentario
